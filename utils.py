8a949ebb6ad77c35270c58e046c990be42f26768
Jonathan Saragosti <saragostij+github@gmail.com>
2022-12-16 15:57:49 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def add_contact_id(df: pd.DataFrame) -> pd.DataFrame:
+    # Create contact ids
+    df["contact_id"] = (
+        df["game_play"]
+        + "_"
+        + df["step"].astype("str")
+        + "_"
+        + df["nfl_player_id_1"].astype("str")
+        + "_"
+        + df["nfl_player_id_2"].astype("str")
+    )
+    return df
+
+
+def expand_contact_id(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Splits out contact_id into seperate columns.
+    """
+    df["game_play"] = df["contact_id"].str[:12]
+    df["step"] = df["contact_id"].str.split("_").str[-3].astype("int")
+    df["nfl_player_id_1"] = df["contact_id"].str.split("_").str[-2]
+    df["nfl_player_id_2"] = df["contact_id"].str.split("_").str[-1]
+    return df
