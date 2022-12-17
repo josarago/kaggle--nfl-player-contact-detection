95b7dabe621044e716fcd4218bc737824937d041
Jonathan Saragosti <saragostij+github@gmail.com>
2022-12-17 00:43:26 -0500

    add submission file constant

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ TRAIN_LABELS_PATH = os.path.join(BASE_DIR, "train_labels.csv")
 
 TEST_TRACKING_PATH = os.path.join(BASE_DIR, "test_player_tracking.csv")
 SUBMISSION_REF_PATH = os.path.join(BASE_DIR, "sample_submission.csv")
+SUBMISSION_FILE_NAME = "submission.csv"
 
 MIN_X_POSITION = 0 
 MAX_X_POSITION = 120
